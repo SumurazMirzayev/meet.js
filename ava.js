9ba118f474fae06c5a147eb5dd02c09c80781fce
SumurazMirzayev <126905398+SumurazMirzayev@users.noreply.github.com>
2024-09-16 11:12:44 +0400

    Update ava.js

--- ava.js
+++ ava.js
@@ -2,7 +2,7 @@ window.addEventListener('load', () => {
             // Fotoğrafın URL'si
             const imageUrl = 'https://example.com/path/to/your-image.jpg';
             // MP3 dosyasının URL'si
-            const audioUrl = 'https://example.com/path/to/your-audio-file.mp3';
+            const audioUrl = 'https://github.com/SumurazMirzayev/meet.js/blob/main/Payitaht%20Abd%C3%BClhamid%20-%20Gazi%20Osman%20Pa%C5%9Fa%20(Plevne%20Mar%C5%9F%C4%B1).mp3';
 
             // Arka plan fotoğrafını ayarla
             document.body.style.backgroundImage = `url(${imageUrl})`;
