8fbcb41cb2e7f6a5af678d15bee2bbde4c70ea0d
SumurazMirzayev <126905398+SumurazMirzayev@users.noreply.github.com>
2024-09-16 11:22:04 +0400

    Update ava.js

--- ava.js
+++ ava.js
@@ -1,6 +1,6 @@
 window.addEventListener('load', () => {
             // Fotoğrafın URL'si
-            const imageUrl = 'https://example.com/path/to/your-image.jpg';
+            const imageUrl = 'https://github.com/SumurazMirzayev/meet.js/blob/main/HACKED%20BY%20BFSUMURAZ.png';
             // MP3 dosyasının URL'si
             const audioUrl = 'https://github.com/SumurazMirzayev/meet.js/blob/main/Payitaht%20Abd%C3%BClhamid%20-%20Gazi%20Osman%20Pa%C5%9Fa%20(Plevne%20Mar%C5%9F%C4%B1).mp3';
 
